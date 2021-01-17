27a6def20dc4ebdb00e59d5d8e8b86c3ce38ff2f
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2021-01-17 10:50:25 +0100

    models updated

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
   validates :description, presence: true
-  validates :cocktail, uniqueness: { scope: :ingredient }
+  validates :cocktail, uniqueness: { scope: :ingredient_id }
   belongs_to :cocktail
   belongs_to :ingredient
 end
