d245bdcc45c57799b91ae9387b54b5e399a9e649
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2021-01-17 11:37:09 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ puts "Deleting ingredients..."
 Ingredient.destroy_all
 puts "Ingredients deleted"
 
-# puts "Deleting cocktails..."
-# Cocktail.destroy_all
-# puts "Cocktails deleted"
+puts "Deleting cocktails..."
+Cocktail.destroy_all
+puts "Cocktails deleted"
 
 puts "Creating ingredients"
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
