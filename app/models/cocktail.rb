22193d1386a86c52e69e19a38d19283bd32be8b6
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2021-01-20 20:30:31 +0100

    pg search cokctails and ingredients in navbar

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,14 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name ],
+    associated_against: {
+      ingredients: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
